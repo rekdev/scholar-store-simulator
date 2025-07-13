68a328fa6d3a6961c32eca821843073fc1647794
Diego Torres <diego.torres.itid@unipolidgo.edu.mx>
2025-07-13 17:30:31 -0600

    Version 1.1 - app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from welcome_menu import show_welcome_menu
 from product_menu import show_products_menu, products
 from product_selection import selection_menu
+from cashout import simulate_payment 
 
 student_name = show_welcome_menu()
 show_products_menu()
 selection = selection_menu(products)
+simulate_payment(selection)

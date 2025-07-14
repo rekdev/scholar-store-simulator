e79c33af8b3cdb2dafb27b1f1c43e102790c26b9
Arturo Lopez <arturolopezsimental81@gmail.com>
2025-07-13 21:05:10 -0600

    Version 1.2 - app.py

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from welcome_menu import show_welcome_menu
 from product_menu import show_products_menu, products
 from product_selection import selection_menu
 from cashout import simulate_payment 
+from summary import show_purchase_summary
+
 
 student_name = show_welcome_menu()
 show_products_menu()
 selection = selection_menu(products)
 simulate_payment(selection)
+show_purchase_summary(student_name, selection)
